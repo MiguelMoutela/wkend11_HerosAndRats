f3524dd32aef0a56701db6d8e18572bf008de6c7
Miguel <miguelmoutela@hotmail.com>
2017-12-08 17:40:05 +0000

    mvp; dificulty is now a word

--- hero.js
+++ hero.js
@@ -9,7 +9,27 @@ Hero.prototype.introduction = function(){
   return `Hi, my name is ${this.name}.`;
 }
 Hero.prototype.eat = function(food){
-  this.health += food.replenishment;
+  if (food.name === this.favouriteFood) {
+    this.health += (1.5 * food.replenishment);
+  }
+  else {
+    this.health += food.replenishment;
+  }
 }
 
+Hero.prototype.listByDificulty = function(){
+  return this.tasks.sort(function(task1, task2){
+    return task2.dificultyLevel - task1.dificultyLevel;
+  })
+}
+Hero.prototype.listByUrgency = function(){
+  return this.tasks.sort(function(task1, task2){
+    return task2.urgencyLevel - task1.urgencyLevel;
+  })
+}
+Hero.prototype.listByReward = function(){
+  return this.tasks.sort(function(task1, task2){
+    return task2.reward - task1.reward;
+  })
+}
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -7,10 +7,16 @@ describe('hero', function(){
   let hero;
   let task;
   let cheese;
+  let chips;
+
   beforeEach(function(){
     hero = new Hero('Me', 'chips');
     task = new Task(1,1,1,false);
+    aDificultTask = new Task(2,1,1,false);
+    anUrgentTask = new Task(1,2,1,false);
+    aRewardingTask = new Task(1,1,2,false);
     cheese = new Food('cheese', 1);
+    chips = new Food('chips', 1);
 });
   it('should have a name',function(){
     assert.strictEqual(hero.name, 'Me')
@@ -30,5 +36,33 @@ describe('hero', function(){
   it('should be able to eat food, and health should go up', function(){
     hero.eat(cheese);
     assert.strictEqual(hero.health, 101);
+  });
+  it('should be able to eat fav food, and health should go up by 1.5 times the replenishment val', function(){
+    hero.eat(chips);
+    assert.strictEqual(hero.health, 101.5);
+  });
+  it('should have a list sorted by dificulty', function(){
+    hero.tasks.push(anUrgentTask);
+    hero.tasks.push(aDificultTask);
+    hero.tasks.push(aRewardingTask);
+    const tasksByDificulty = hero.listByDificulty();
+    const mostDificultTask = tasksByDificulty[0];
+    assert.strictEqual(mostDificultTask, aDificultTask)
+  })
+  it('should have a list sorted by urgency', function(){
+    hero.tasks.push(aDificultTask);
+    hero.tasks.push(anUrgentTask);
+    hero.tasks.push(aRewardingTask);
+    const tasksByUrgency = hero.listByUrgency();
+    const mostUrgentTask = tasksByUrgency[0];
+    assert.strictEqual(mostUrgentTask, anUrgentTask)
+  })
+  it('should have a list sorted by reward', function(){
+    hero.tasks.push(aDificultTask);
+    hero.tasks.push(anUrgentTask);
+    hero.tasks.push(aRewardingTask);
+    const tasksByReward = hero.listByReward();
+    const mostRewardingTask = tasksByReward[0];
+    assert.strictEqual(mostRewardingTask, aRewardingTask)
   })
 });
