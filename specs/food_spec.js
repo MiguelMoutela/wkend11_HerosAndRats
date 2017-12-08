2d2cc5f69858a26e972ecd6e81dfa78ae2c8dcd6
Miguel <miguelmoutela@hotmail.com>
2017-12-08 15:55:10 +0000

    food tests passing

---
+++ specs/food_spec.js
@@ -0,0 +1,15 @@
+const Food = require('../food.js');
+const assert = require('assert');
+
+describe('Food', function(){
+  let food;
+  beforeEach(function (){
+    food = new Food('cheese',1);
+  });
+  it('should have a name', function (){
+    assert.strictEqual(food.name, 'cheese');
+  })
+  it('should have a replenishment', function (){
+    assert.strictEqual(food.replenishment, 1);
+  })
+})
